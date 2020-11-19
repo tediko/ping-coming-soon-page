df1a9bbd06e3432b48b5ea1e22eda6f0c4c73314
tediko <tediko123@wp.pl>
2020-11-19 09:13:37 +0100

    Simplified if statement in validateForm

--- js/app.js
+++ js/app.js
@@ -6,12 +6,7 @@ const validateForm = (event) => {
     event.preventDefault();
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!emailRegex.test(input.value)) {
-        form.classList.add('active');
-    } else {
-        form.submit();
-    }
-    
+    !emailRegex.test(input.value) ? form.classList.add('active') : form.submit();
 }
 
 submitBtn.addEventListener('click', validateForm);
